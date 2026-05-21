17524a4ff8669ddffbe016ef7a9dfeeeec22d12c
Jean-jacques-25 <jeanjacquesaguin30@gmail.com>
2026-05-21 05:32:55 +0000

    Standardisation : dossier renommé en src et point d'entrée app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from app import create_app
-
+from src import create_app
 app = create_app()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 3000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port, debug=True)
