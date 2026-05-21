bfe60b350c61e292b33aee4e6513bd2a234819c7
Jean-jacques-25 <jeanjacquesaguin30@gmail.com>
2026-05-21 05:57:05 +0000

    Restoration : Structure originale Databroker clean

--- app.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-app = create_app()
-
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=port, debug=True)
